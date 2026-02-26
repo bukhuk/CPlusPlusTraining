5f6a6bf435ae7d4d4e7a638bacb90c965ba6b746
bukhuk <iharbychyk@gmail.com>
2026-02-26 17:58:28 +0300

    Write a stress test for ArenaAllocator

--- Arena/main.cpp
+++ Arena/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include <string>
+#include <list>
 
-#include "arena.h"
+#include "arena_allocator.h"
 
 int main() {
-	Arena arena(1024 * 1024);
-	auto ptr = arena.alloc<std::string>("Hello world!");
-	std::cout << *ptr << std::endl;
+	const int iterations = 100'000;
+	
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+
+		std::list<int> std_list;
+		for (int i = 0; i < iterations; i++) {
+			std_list.push_back(i);
+		}
+
+		auto end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = end - start;
+		std::cout << "Standart std::list: " << diff.count() << std::endl;
+	}
+
+	{
+		Arena arena(1 << 25);
+		ArenaAllocator<int> alloc(arena);
+
+		auto start = std::chrono::high_resolution_clock::now();
+
+		std::list<int, ArenaAllocator<int>> arena_list(alloc);
+
+		for (int i = 0; i < iterations; i++) {
+			arena_list.push_back(i);
+		}
+
+		auto end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> diff = end - start;
+		std::cout << "Arena std::list: " << diff.count() << std::endl;
+	}
+
 	return 0;
 }
